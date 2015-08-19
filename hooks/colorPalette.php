7b7df8c8327ffec65ba9734a0edf2f0325e10ace
Makoto <FujiMakoto@users.noreply.github.com>
2015-08-19 19:46:22 -0400

    Template hook updates

--- hooks/colorPalette.php
+++ hooks/colorPalette.php
@@ -3,6 +3,10 @@
 class colorpalette_hook_colorPalette extends _HOOK_CLASS_
 {
 
+/**
+ * The number of hoops we had to jump through here just to keep things properly ordered is a bit ridiculous
+ */
+
 /* !Hook Data - DO NOT REMOVE */
 public static function hookData() {
  return array_merge_recursive( array (
@@ -10,28 +14,30 @@ public static function hookData() {
   array (
     0 => 
     array (
-      'selector' => '#elGalleryImageStats > [data-role=\'imageStats\']',
-      'type' => 'add_inside_end',
-      'content' => '{{if $image->color_palette}}
-	{{if settings.cpalette_show_dominant and $image->dominant_color}}
-		<h2 class="ipsType_minorHeading">{lang="dominant_color"}</h2>
-		<div class="swatches">
-			<div class="swatch swatch_tiny swatch_dominant {{if $image->dominant_color_lightness >= 85}}swatch_light{{else}}swatch_dark{{endif}}" style="background-color: {$image->dominant_color}">
-				{{if $image->dominant_color_name}}
-					{lang="cpalette_color_{$image->dominant_color_name}"}
-				{{endif}}
-			</div>
-		</div>
-		<br>
-	{{endif}}
-	{{if settings.cpalette_show_palette and $image->color_palette}}
-		<h2 class="ipsType_minorHeading">{lang="color_palette"}</h2>
-		<div class="swatches">
-			{{foreach $image->color_palette as $color}}
-				<div class="swatch swatch_tiny" style="background-color: {$color}"></div>
-			{{endforeach}}
-		</div>
-	{{endif}}
+      'selector' => '#elGalleryImageStats > div[data-role=\'imageStats\'] > div:has(\'ul.ipsList_inline > li > i.fa-comments\')',
+      'type' => 'add_after',
+      'content' => '{{if (settings.cpalette_show_dominant and $image->dominant_color) or (settings.cpalette_show_palette and $image->color_palette)}}
+	<hr class=\'ipsHr\'>
+{{endif}}
+{template="colorPalette" app="colorpalette" group="gallery" params="$image"}',
+    ),
+    1 => 
+    array (
+      'selector' => '#elGalleryImageStats > div[data-role=\'imageStats\'] > div.ipsType_center:first',
+      'type' => 'add_after',
+      'content' => '{{if !($image->directContainer() instanceof \IPS\gallery\Album)}}
+    {{if (settings.cpalette_show_dominant and $image->dominant_color) or (settings.cpalette_show_palette and $image->color_palette)}}
+        <hr class=\'ipsHr\'>
+    {{endif}}
+	{template="colorPalette" app="colorpalette" group="gallery" params="$image"}
+{{endif}}',
+    ),
+    2 => 
+    array (
+      'selector' => '#elGalleryImageStats > div[data-role=\'imageStats\']',
+      'type' => 'add_inside_start',
+      'content' => '{{if !($image->directContainer() instanceof \IPS\gallery\Album) and !$image->container()->allow_rating}}
+	{template="colorPalette" app="colorpalette" group="gallery" params="$image"}
 	{{if (settings.cpalette_show_dominant and $image->dominant_color) or (settings.cpalette_show_palette and $image->color_palette)}}
 		<hr class=\'ipsHr\'>
 	{{endif}}
@@ -41,8 +47,5 @@ public static function hookData() {
 ), parent::hookData() );
 }
 /* End Hook Data */
-
-
-
 
 }
